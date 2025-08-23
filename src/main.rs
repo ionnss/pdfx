522186775b3fd43b19719efd241db479e8f5070a
ionnss <39104500+ionnss@users.noreply.github.com>
2025-08-23 13:43:53 -0300

    Update dependencies and enhance PDF search functionality with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 // Main.rs
 
+use std::path::Path;
 use walkdir::WalkDir;
 
-fn main() {
 
-    // Get the current directory
-    let current_dir = std::env::current_dir().unwrap();
-    println!("Searching for PDF files in: {:?}", current_dir.display());
+fn main() {
+    
+    // Get command line arguments
+    let args: Vec<String> = std::env::args().collect();
+    // Check if user provided a directory
+    let current_dir = if args.len() > 1 {
+        Path::new(&args[1]).to_path_buf()
+    } else {
+        std::env::current_dir().unwrap()
+    };
+
+    println!("\nSearching for PDF files in: {:?}\n", current_dir.display());
 
     // Create a vector to store pdf files paths
     let mut pdf_files = Vec::new();
 
     // Iterate over the directory and subdirectories
     for entry in WalkDir::new(current_dir) {
-        let entry = entry.unwrap();
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(_) => continue,
+        };
         let path = entry.path();
 
         if let Some(ext) = path.extension() {
@@ -25,5 +37,7 @@ fn main() {
 
     for pdf_file in pdf_files {
         println!("{:?}", pdf_file);
+    } if pdf_files.is_empty() {
+        println!("No PDF files foun")
     }
 }
